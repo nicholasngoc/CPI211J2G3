28f1580c01caa76b33e8c929cb22afe09311f6c0
Nick N <nicholas.ngoc.nguyen@gmail.com>
2020-02-28 19:16:17 -0700

    Heath and Gun Improvements

--- Assets/Scripts/Guns/Shotgun.cs
+++ Assets/Scripts/Guns/Shotgun.cs
@@ -7,29 +7,35 @@ using UnityEngine;
 /// </summary>
 public class Shotgun : Gun
 {
+    [Header("Shotgun Stuff")]
     public float forceModifier;    //This influences the force added onto the bullet on creation
     public float initRadius;    //This influences the position that the bullet spawn from the bulletSpawnPoint
 
     public override void Shoot()
     {
-        for (int x = 0; x < bulletCount; x++)
-        {  
-            //Random angle and radius to for a spawn position
-            float randRadius = Random.Range(0, initRadius);
-            float randAngle = Random.Range(0, Mathf.PI * 2);
-
-            //Spawns bullet within a circle around the bullet spawn point
-            Vector3 spawnPos = bulletSpawnPoint.position + (bulletSpawnPoint.right * (randRadius * Mathf.Cos(randAngle))) + (bulletSpawnPoint.up * (randRadius * Mathf.Sin(randAngle)));
-
-            GameObject bullet = Instantiate(bulletPrefab, spawnPos, bulletSpawnPoint.rotation);
-
-            Rigidbody rb = bullet.GetComponent<Rigidbody>();
-            if (rb != null)
+        if(clipCount > 0)
+        {
+            for (int x = 0; x < bulletCount; x++)
             {
-                //Modified force the bullet will receive
-                Vector3 direction = bulletSpawnPoint.forward + new Vector3(Random.Range(-forceModifier, forceModifier), Random.Range(-forceModifier, forceModifier), 0);
-                rb.AddForce(direction * bulletSpeed);
+                //Random angle and radius to for a spawn position
+                float randRadius = Random.Range(0, initRadius);
+                float randAngle = Random.Range(0, Mathf.PI * 2);
+
+                //Spawns bullet within a circle around the bullet spawn point
+                Vector3 spawnPos = bulletSpawnPoint.position + (bulletSpawnPoint.right * (randRadius * Mathf.Cos(randAngle))) + (bulletSpawnPoint.up * (randRadius * Mathf.Sin(randAngle)));
+
+                GameObject bullet = Instantiate(bulletPrefab, spawnPos, bulletSpawnPoint.rotation);
+
+                Rigidbody rb = bullet.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    //Modified force the bullet will receive
+                    Vector3 direction = bulletSpawnPoint.forward + new Vector3(Random.Range(-forceModifier, forceModifier), Random.Range(-forceModifier, forceModifier), 0);
+                    rb.AddForce(direction * bulletSpeed);
+                }
             }
+
+            clipCount--;
         }
     }
 }

f5ef8d300222b8b17cfb7c6595065c8bd811727c
Nick N <nicholas.ngoc.nguyen@gmail.com>
2020-02-28 18:31:09 -0700

    Imported assets and adjusted guns/bullets

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float timeToDestroy;
+
+    private void Start()
+    {
+        StartCoroutine(DestroyTimer());
+    }
+
+    private IEnumerator DestroyTimer()
+    {
+        yield return new WaitForSeconds(timeToDestroy);
+
+        Destroy(gameObject);
+    }
+}

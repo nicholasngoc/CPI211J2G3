74f58d576de58df443c3ef33d029045084ef32e1
Nick N <nicholas.ngoc.nguyen@gmail.com>
2020-02-28 20:04:24 -0700

    Simple AI

---
+++ Assets/Scripts/SimpleAI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+/// <summary>
+/// Simple class for an AI. It constantly follows a target
+/// </summary>
+public class SimpleAI : MonoBehaviour
+{
+    public NavMeshAgent NavAgent
+    {
+        get
+        {
+            return GetComponent<NavMeshAgent>();
+        }
+    }
+    public Transform currentTarget;
+    public float redirectDelay;
+
+    private void Start()
+    {
+        StartCoroutine(RedirectRoutine());
+    }
+
+    /// <summary>
+    /// This coroutine continuosly adjusts the AI's destination
+    /// with a delay (this helps performance)
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator RedirectRoutine()
+    {
+        while(true)
+        {
+            NavAgent.SetDestination(currentTarget.position);
+
+            yield return new WaitForSeconds(redirectDelay);
+        }
+    }
+}

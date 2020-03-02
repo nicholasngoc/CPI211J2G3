4ec8cc65e6e412742fc736f0d1d21eaaab202a7c
Daniel <danieltherose@yahoo.com>
2020-03-01 20:35:41 -0700

    Enemies Coming for Ya

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -9,17 +9,28 @@ public class EnemyHealth : MonoBehaviour
 {
     public int health;
 
+    private AudioSource audio;
+
     public WaveSpawner spawner; //Reference to the spawner that created this enemy
 
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
     private void OnCollisionEnter(Collision collision)
     {
         //If hit by a bullet and I am out of health kill me
         if(collision.gameObject.CompareTag("Bullet"))
         {
             health--;
+            audio.Play();
 
             if (health <= 0)
+            {
+                audio.Play();
                 Destroy(gameObject);
+            }
+
         }
     }
 
